684817d0f8286af94860c8bcb9a29729e73ada59
candysan7 <86620651+candysan7@users.noreply.github.com>
2022-07-11 12:10:20 -0700

    Add files via upload

--- apps/home_carbon.py
+++ apps/home_carbon.py
@@ -50,11 +50,24 @@ def app():
     # st.write(carbon_usage)
 
     if st.button('Calculate My Carbon Emission!'): 
+
+        average_carbon = 160 ### not sure of this value 
     # if st.form_submit_button('Calculate My Carbon Emission!'):
-        carbon_emission = str(round(miles_drive*carbon_usage,2))
-        display_emission = "Your trip added " + carbon_emission + " grams of C02 to the atmosphere"
+        carbon_emission = round(miles_drive*carbon_usage,2)
+        lb_to_gram = (1/453.592) ####(lbs/grams)
+        carbon_emission = carbon_emission * lb_to_gram # convert from grams to lbs 
+        carbon_emission = str(round(carbon_emission,2))
+        display_emission = "Your trip added " + carbon_emission + " pounds(lbs) of C02 to the atmosphere"
         st.subheader(display_emission)
-        
-        st.markdown('$this is 90% higher than$')
-        st.markdown('$this is 90 higher than the us average __ $')
-        st.success('50% lower than average')
+
+        if miles_drive: 
+            percent_diff = (average_carbon - carbon_usage) / average_carbon
+            percent_diff = round((percent_diff*100),2)
+    
+
+            if percent_diff >=0: 
+                percent_diff= str(percent_diff)
+                st.success(percent_diff+'%'+ ' lower than U.S. average')
+            else: 
+                percent_diff= str(percent_diff)
+                st.error(percent_diff+'%' + ' higher than U.S. average')
